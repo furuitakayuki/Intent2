f24d439ad86f1e26d6025b3af331b97f78e89eb8
furuitakayuki <maki1904@icloud.com>
2022-09-25 20:36:05 +0900

    インテント連携

--- app/src/main/java/jp/techacademy/takayuk/intent2/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takayuk/intent2/MainActivity.kt
@@ -2,10 +2,21 @@ package jp.techacademy.takayuk.intent2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, editText.text.toString())
+            startActivity(intent)
+        }
     }
 }
